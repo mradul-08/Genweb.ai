ae53678b3a71934bef4de67681377a0466885a2e
Madul-garg <mradulgarg2005@gmail.com>
2026-06-15 01:00:39 +0530

    fix: authDomain to vercel domain

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -3,7 +3,7 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "genwebai-dca16.firebaseapp.com",
+  authDomain: "genweb-ai-delta.vercel.app",
   projectId: "genwebai-dca16",
   storageBucket: "genwebai-dca16.firebasestorage.app",
   messagingSenderId: "350304621044",
